855091ef6ca857410a285be0ffc6070116d6ce14
na-go <kn.mbk201.mynk@gmail.com>
2023-02-07 09:36:41 +0900

    削除

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloNago = void 0;
-const helloNago = () => {
-    console.log('hello, Nago.');
-};
-exports.helloNago = helloNago;
